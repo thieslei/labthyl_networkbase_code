15665488e43e5f18ee870e51eea1cfc58d077017
Thieslei Oliveira <thieslei@gmail.com>
2021-07-02 15:07:21 -0300

    add new subnet zone b

--- modules/network/subnets.tf
+++ modules/network/subnets.tf
@@ -2,7 +2,7 @@
 #######################
 # Subnets
 #######################
-
+#SubnetPublic-A
 resource "aws_subnet" "subnet_public_a" {
     vpc_id            = aws_vpc.vpc_main.id
     availability_zone = "us-east-1a"
@@ -14,10 +14,23 @@ resource "aws_subnet" "subnet_public_a" {
     }
 }
 
+#SubnetPublic-B
+resource "aws_subnet" "subnet_public_b" {
+    vpc_id            = aws_vpc.vpc_main.id
+    availability_zone = "us-east-1b"
+    cidr_block        = "10.10.2.0/24"
+    map_public_ip_on_launch = true
+    
+    tags = {
+         Name    = "subnet-public-b-${var.project}"
+    }
+}
+
+#SubnetPublic-C
 resource "aws_subnet" "subnet_public_c" {
     vpc_id            = aws_vpc.vpc_main.id
     availability_zone = "us-east-1c"
-    cidr_block        = "10.10.1.0/24"
+    cidr_block        = "10.10.3.0/24"
     map_public_ip_on_launch = true
     
     tags = {
