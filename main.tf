8177673f948c9d612ce456901ac53cfbe30059cd
Thieslei Oliveira <thieslei@gmail.com>
2021-07-21 18:29:55 -0300

    Ajuste Modulo

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ provider "aws" {
 }
 
 module "vpc01" {
-  source       = "./modules/vpc01"
+  source       = "./modules/vpc_01"
   project = var.project
 }
 
 module "vpc02" {
-  source       = "./modules/vpc02"
+  source       = "./modules/vpc_02"
   project = var.project
 }
